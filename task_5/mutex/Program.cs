94c44b62a031591b4f8af5c646f7024314debcd1
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2025-08-07 17:00:51 +0300

    Create Program.cs

---
+++ task_5/mutex/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    private static Mutex _logMutex = new Mutex(false, "LogMutex");
+    private static string _logFilePath = "log.txt";
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            ThreadPool.QueueUserWorkItem(LogMessage, i);
+        }
+        Thread.Sleep(2000);
+    }
+
+    static void LogMessage(object state)
+    {
+        int threadId = (int)state;
+        try
+        {
+            bool acquired = _logMutex.WaitOne(TimeSpan.FromSeconds(1), false);
+
+            if (acquired)
+            {
+                using (StreamWriter writer = new StreamWriter(_logFilePath, true))
+                {
+                    writer.WriteLine($"Thread {threadId} logged a message at {DateTime.Now}");
+                    Thread.Sleep(100);
+                }
+
+                _logMutex.ReleaseMutex();
+            }
+            else
+            {
+                Console.WriteLine($"Err: timeout. Thread {threadId} could not acquire the mutex");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Err: Thread {threadId} : {ex.Message}");
+        }
+    }
+}
